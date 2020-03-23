bc01567cf093aaf9c3d461a636ff82cfcf3bdb7b
Mateusz Konieczny <matthew.the.equine@gmail.com>
2020-03-23 23:50:19 +0100

    dayOfWeek + hours

--- src/App.svelte
+++ src/App.svelte
@@ -4,11 +4,15 @@
   const date = new Date()
   const dayOfYear = date.getDayOfYear()
   const yearOfLife = date.getFullYear()
+  const dayOfWeek = date.getDay()
+  const hours = date.getHours()
 </script>
 
 <main>
   <Progression total={83} current={yearOfLife - 1992} color="red"/>
   <Progression total={365} current={dayOfYear} color="green" />
+  <Progression total={7} current={dayOfWeek} color="yellow" />
+  <Progression total={24} current={hours} color="blue" />
 </main>
 
 <style>
